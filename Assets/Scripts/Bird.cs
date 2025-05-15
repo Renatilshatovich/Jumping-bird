6cbc9e8f668a06bfe644f07d3a07b997d9c31747
Renat Husnetdinov <renatilshatovich@yandex.ru>
2025-05-15 20:59:35 +0500

    Added GetComponent

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -1,13 +1,17 @@
+using System;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Bird : MonoBehaviour
-{
-    [SerializeField] private Vector3 jumpForce;
-    [SerializeField] private Rigidbody _rigidbody;
+{ 
+    [SerializeField] private Vector3 _jumpForce;
+    private Rigidbody _rigidbody;
+
+    private void Awake() => _rigidbody = GetComponent<Rigidbody>();
 
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space)) 
-            _rigidbody.AddForce(jumpForce, ForceMode.Impulse);
+            _rigidbody.AddForce(_jumpForce, ForceMode.Impulse);
     }
 }
